cc4b281761029a4a81a656841c3603ca56b6b19e
John Cleary <john@createk.io>
2019-06-12 19:17:19 +0100

    nest the CMS under the /admin route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  comfy_route :cms_admin, path: "/admin"
+  comfy_route :cms_admin, path: "/admin/cms"
   # Ensure that this route is defined last
   comfy_route :cms, path: "/"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
